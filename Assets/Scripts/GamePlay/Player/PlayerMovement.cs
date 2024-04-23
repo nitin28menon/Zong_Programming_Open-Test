082511158511705e8b4d25cfcd6789a15beebc9d
Nitin Menon <nitin1996menon@gmail.com>
2024-04-23 23:58:46 +0530

    add: player movement script for controlling player movement

---
+++ Assets/Scripts/GamePlay/Player/PlayerMovement.cs
@@ -0,0 +1,207 @@
+using UnityEngine;
+using ZPOT.Constant;
+
+namespace ZPOT.GamePlay
+{
+    /// <summary>
+    /// class for player movement
+    /// </summary>
+    [RequireComponent(typeof(Rigidbody))]
+    public class PlayerMovement : MonoBehaviour
+    {
+        #region Serialize fields
+
+        [SerializeField] private Transform orientation;
+        [SerializeField] private LayerMask groundLayer;
+
+        #endregion
+
+        #region Private variables
+
+        //movement variables
+        private float groundDrag;
+        private float playerHeight;
+        private float playerMoveSpeed;
+        private float groundForce = 10f;
+        private bool isGrounded;
+        private bool playerCanMove;
+        private Vector3 moveDirection;
+        private Rigidbody playerRB;
+
+        //jump variables
+        private float playerJumpForce;
+        private float playerJumpCoolddown;
+        private float airMultiplier;
+        private bool readyToJump;
+        private KeyCode jumpKey;
+
+        //player input variables
+        private float verticalInput;
+        private float horizontalInput;
+
+        #endregion
+
+        #region Internal Variables
+
+        internal bool PlayerCanMove { get => playerCanMove; set => playerCanMove = value; }
+
+        #endregion
+
+        #region Mono methods
+
+        private void Update()
+        {
+            if (!playerCanMove)
+                return;
+            CheckForPlayerInput();
+            SpeedControl();
+            HandleDrag();
+        }
+
+        private void FixedUpdate()
+        {
+            if (!playerCanMove)
+                return;
+            MovePlayer();
+        }
+
+        #endregion    
+
+        #region Initialising functions
+
+        /// <summary>
+        /// Initialises script
+        /// </summary>
+        internal void Init()
+        {
+            InitialiseComponents();
+            InitialiseVariables();
+        }
+
+        /// <summary>
+        /// Initialises components
+        /// </summary>
+        private void InitialiseComponents()
+        {
+            playerRB = GetComponent<Rigidbody>();
+            playerRB.freezeRotation = true;
+        }
+
+        /// <summary>
+        /// Initialises variables
+        /// </summary>
+        private void InitialiseVariables()
+        {
+            playerMoveSpeed = Constants.PlayerMoveSpeed;
+            jumpKey = Constants.PlayerJumpKey;
+            playerHeight = Constants.PlayerHeight;
+            groundDrag = Constants.GroundDrag;
+            playerJumpForce = Constants.PlayerJumpForce;
+            playerJumpCoolddown = Constants.PlayerJumpCoolddown;
+            airMultiplier = Constants.AirMultiplier;
+            playerMoveSpeed = Constants.PlayerMoveSpeed;
+            isGrounded = true;
+            readyToJump = true;
+            playerCanMove = true;
+        }
+
+        #endregion
+
+        #region Player input functions
+
+        /// <summary>
+        /// gets the user's input
+        /// </summary>
+        private void CheckForPlayerInput()
+        {
+            CheckForPlayerMovement();
+            CheckForPlayerJump();
+        }
+
+        /// <summary>
+        /// checks for input regarding player movement
+        /// </summary>
+        private void CheckForPlayerMovement()
+        {
+            horizontalInput = Input.GetAxisRaw("Horizontal");
+            verticalInput = Input.GetAxisRaw("Vertical");
+        }
+
+        /// <summary>
+        /// checks for input regarding player jump
+        /// </summary>
+        private void CheckForPlayerJump()
+        {
+            if (Input.GetKey(jumpKey) && readyToJump && CheckForIsGrounded())
+            {
+                readyToJump = false;
+                Jump();
+                Invoke(nameof(ResetJump), playerJumpCoolddown);
+            }
+        }
+
+        #endregion
+
+        /// <summary>
+        /// checks if the player is grounded or not
+        /// </summary>
+        private bool CheckForIsGrounded()
+        {
+            return isGrounded = Physics.Raycast(transform.position, Vector3.down, playerHeight * 0.5f + 0.2f, groundLayer);
+        }
+
+        #region Player movement functions
+
+        /// <summary>
+        /// moves the player
+        /// </summary>
+        private void MovePlayer()
+        {
+            //calculate movement direction
+            moveDirection = orientation.forward * verticalInput + orientation.right * horizontalInput;
+            //add force based on wether the player is on ground or in air
+            playerRB.AddForce((CheckForIsGrounded() ? groundForce : groundForce * airMultiplier) * playerMoveSpeed * moveDirection.normalized, ForceMode.Force);
+        }
+
+        /// <summary>
+        /// controls speed of player
+        /// </summary>
+        private void SpeedControl()
+        {
+            Vector3 currentVelocity = new Vector3(playerRB.velocity.x, 0f, playerRB.velocity.z);
+
+            //limit velocity
+            if (currentVelocity.magnitude > playerMoveSpeed)
+            {
+                Vector3 limitedVelocity = currentVelocity.normalized * playerMoveSpeed;
+                playerRB.velocity = new Vector3(limitedVelocity.x, playerRB.velocity.y, limitedVelocity.z);
+            }
+        }
+
+        /// <summary>
+        /// handles drag for player movement
+        /// </summary>
+        private void HandleDrag() => playerRB.drag = CheckForIsGrounded() ? groundDrag : 0;
+
+        #endregion
+
+        #region Player jump related functions
+
+        /// <summary>
+        /// makes the player jump
+        /// </summary>
+        private void Jump()
+        {
+            // reset y velocity before jumping
+            playerRB.velocity = new Vector3(playerRB.velocity.x, 0, playerRB.velocity.z);
+            playerRB.AddForce(transform.up * playerJumpForce, ForceMode.Impulse);
+        }
+
+        /// <summary>
+        /// resets jump
+        /// </summary>
+        private void ResetJump() => readyToJump = true;
+
+        #endregion
+    }
+}
