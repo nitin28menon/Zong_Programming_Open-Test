052014cde0b81483eae20aa6630a02dbb0d29280
Nitin Menon <nitin1996menon@gmail.com>
2024-04-23 23:59:48 +0530

    update: scripts for player camera movement

---
+++ Assets/Scripts/GamePlay/Camera/PlayerCamera.cs
@@ -0,0 +1,93 @@
+using UnityEngine;
+using ZPOT.Constant;
+
+namespace ZPOT.GamePlay
+{
+    /// <summary>
+    /// class for player camera movement
+    /// </summary>
+    public class PlayerCamera : MonoBehaviour
+    {
+        #region SerializeFields
+
+        [SerializeField] private Transform orientaion;
+
+        #endregion
+
+        #region Private variables
+
+        private float xSensitivity, ySensitivity;
+        private float xRotation, yRotation;
+        private float xInputValue, yInputValue;
+        private bool canRotateCamera;
+
+        #endregion
+
+        #region Internal variables
+
+        internal bool CanRotateCamera { get => canRotateCamera; set => canRotateCamera = value; }
+
+        #endregion
+
+        #region Mono methods
+
+        private void Update()
+        {
+            if(!canRotateCamera)
+                return;
+            GetMouseInput();
+            SetCameraRotation();
+            RotateCamera();
+        }
+
+        #endregion
+
+        /// <summary>
+        /// initialises the script
+        /// </summary>
+        internal void Init()
+        {
+            LockMouse();
+            canRotateCamera = true;
+            xSensitivity = Constants.XSensitivity;
+            ySensitivity = Constants.YSensitivity;
+        }
+
+        /// <summary>
+        /// locks the mouse and makes it invisible
+        /// </summary>
+        private void LockMouse()
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+
+        /// <summary>
+        /// gets the mouse input
+        /// </summary>
+        private void GetMouseInput()
+        {
+            xInputValue = Input.GetAxisRaw("Mouse X") * Time.deltaTime * xSensitivity;
+            yInputValue = Input.GetAxisRaw("Mouse Y") * Time.deltaTime * ySensitivity;
+        }
+
+        /// <summary>
+        /// sets the camera rotation
+        /// </summary>
+        private void SetCameraRotation()
+        {
+            yRotation += xInputValue;
+            xRotation -= yInputValue;
+            xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+        }
+
+        /// <summary>
+        /// Rotates the camera
+        /// </summary>
+        private void RotateCamera()
+        {
+            transform.rotation = Quaternion.Euler(xRotation, yRotation, 0);
+            orientaion.rotation = Quaternion.Euler(0, yRotation, 0);
+        }
+    }
+}
